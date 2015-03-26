9e3d0a8389a9212d8e4997dc46ffeb486b092814
kaduaono <cadueao@hotmail.com>
2015-03-26 04:45:04 -0300

    At.2
    At.2

--- js/main.js
+++ js/main.js
@@ -12,10 +12,35 @@ var app = {
             }
         });
     },
+	
+	renderHomeView: function() {
+    	var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+		$('body').html(html);
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+	
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+		var self = this;
+        this.store = new MemoryStore(function(){
+				self.showAlert('Tutorial carregado!!', 'Deu certo!');
+				self.renderHomeView();
+		
+		});
+		
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
